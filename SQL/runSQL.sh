6392ef1ff91b1dd65ba2d9445a9e068be24da435
gsh <gsh1130@sjtu.edu.cn>
2025-09-18 14:58:19 -0400

    Revert "Merge pull request #4 from gaoshenghan1130/pointSys"
    This reverts commit e37ceb750e5ac0e3c8349d8b44cd240c1cf44d87, reversing
    changes made to b2e46995e1ad4a315e20301fc6f74f9590aac0a4.

--- SQL/runSQL.sh
+++
@@ -1,1 +0,0 @@
-mysql -u root -p < ./SQL/createDatabase.sql
--- public/assets/js/pointSys/back2front.js
+++
@@ -1,5 +0,0 @@
-async function getScore(user) {
-  const response = await fetch(`/score/${user}`);
-  const data = await response.json();
-  document.getElementById("score").innerText = `${data.user}: ${data.score}`;
-}
