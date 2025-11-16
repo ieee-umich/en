dd5cdbdcdf1eeab8dbb049b8955ca020425755f5
gsh <gsh1130@sjtu.edu.cn>
2025-11-15 22:50:05 -0500

    log response for testing

--- scripts/verifyToken.js
+++ scripts/verifyToken.js
@@ -13,16 +13,12 @@ async function checkLogin() {
         });
         const data = await res.json();
         console.log(data);
-        if (!data.loggedIn) window.location.href = `${FRONTEND_URL}/login.html`;
-        if (data.loggedIn && !data.decoded.isAdmin) window.location.href = `${FRONTEND_URL}/ui/login/submitFailed.html?error=Not an admin user`;
+        //if (!data.loggedIn) window.location.href = `${FRONTEND_URL}/login.html`;
+        //if (data.loggedIn && !data.decoded.isAdmin) window.location.href = `${FRONTEND_URL}/ui/login/submitFailed.html?error=Not an admin user`;
         // If logged in and is admin, do nothing and allow access
-        
+
     } catch (err) {
         console.log(err);
-        if (err.message === 'Not logged in') { // request login 
-            window.location.href = `${FRONTEND_URL}/login.html`;
-            return;
-        }
         //window.location.href = `${FRONTEND_URL}/ui/login/submitFailed.html?error=${err}`;
     }
 }
